76d7b67b46beb74f3aaa01df86d122e8c87cf43a
lasse <77295879+lassejlv@users.noreply.github.com>
2024-09-07 21:36:23 +0200

    first commit

---
+++ src/components/Tech.tsx
@@ -0,0 +1,176 @@
+import { useState } from 'react';
+import {
+  SiBun,
+  SiCss3,
+  SiDocker,
+  SiExpress,
+  SiFigma,
+  SiGithub,
+  SiGo,
+  SiHtml5,
+  SiJavascript,
+  SiMongodb,
+  SiNetlify,
+  SiNextdotjs,
+  SiNodedotjs,
+  SiPostgresql,
+  SiPrisma,
+  SiRailway,
+  SiReact,
+  SiTailwindcss,
+  SiTypescript,
+  SiUbuntu,
+  SiVercel,
+  SiVisualstudiocode,
+  SiDrizzle,
+  SiHono,
+  SiShadcnui,
+  SiTurso,
+  SiZedindustries,
+} from 'react-icons/si';
+import CoolifyIcon from './CoolifyIcon';
+
+const size = 30;
+
+interface Tech {
+  name: string;
+  icon: JSX.Element;
+}
+
+const techs: Tech[] = [
+  {
+    name: 'Node.js',
+    icon: <SiNodedotjs className="text-green-500" size={size} />,
+  },
+  {
+    name: 'Bun.js',
+    icon: <SiBun className="text-orange-200" size={size} />,
+  },
+  {
+    name: 'React',
+    icon: <SiReact className="text-blue-500" size={size} />,
+  },
+  {
+    name: 'Hono',
+    icon: <SiHono className="text-white hover:text-orange-400" size={size} />,
+  },
+  {
+    name: 'Express.js',
+    icon: <SiExpress className="text-white" size={size} />,
+  },
+  {
+    name: 'Next.js',
+    icon: <SiNextdotjs className="text-white" size={size} />,
+  },
+  {
+    name: 'Shadcn UI',
+    icon: <SiShadcnui className="text-white" size={size} />,
+  },
+  {
+    name: 'Tailwind CSS',
+    icon: <SiTailwindcss className="text-teal-500" size={size} />,
+  },
+  {
+    name: 'CSS',
+    icon: <SiCss3 className="text-blue-500" size={size} />,
+  },
+  {
+    name: 'HTML',
+    icon: <SiHtml5 className="text-orange-500" size={size} />,
+  },
+  {
+    name: 'JavaScript',
+    icon: <SiJavascript className="text-yellow-500" size={size} />,
+  },
+  {
+    name: 'TypeScript',
+    icon: <SiTypescript className="text-blue-500" size={size} />,
+  },
+  {
+    name: 'Go',
+    icon: <SiGo className="text-sky-400" size={size} />,
+  },
+  {
+    name: 'MongoDB',
+    icon: <SiMongodb className="text-green-500" size={size} />,
+  },
+  {
+    name: 'PostgreSQL',
+    icon: <SiPostgresql className="text-blue-400" size={size} />,
+  },
+  {
+    name: 'Turso',
+    icon: <SiTurso className="text-emerald-500" size={size} />,
+  },
+  {
+    name: 'Drizzle ORM',
+    icon: <SiDrizzle className="text-white hover:text-lime-400" size={size} />,
+  },
+  {
+    name: 'Prisma ORM',
+    icon: <SiPrisma className="text-white" size={size} />,
+  },
+  {
+    name: 'Figma',
+    icon: <SiFigma className="text-indigo-400" size={size} />,
+  },
+  {
+    name: 'Github',
+    icon: <SiGithub className="text-white" size={size} />,
+  },
+  {
+    name: 'Zed',
+    icon: <SiZedindustries className="text-white" size={size} />,
+  },
+  {
+    name: 'Visual Studio Code',
+    icon: <SiVisualstudiocode className="text-blue-500" size={size} />,
+  },
+  {
+    name: 'Vercel',
+    icon: <SiVercel className="text-white" size={size} />,
+  },
+  {
+    name: 'Netlify',
+    icon: <SiNetlify className="text-teal-600" size={size} />,
+  },
+  {
+    name: 'Docker',
+    icon: <SiDocker className="text-blue-500" size={size} />,
+  },
+  {
+    name: 'Coolify',
+    icon: <CoolifyIcon />,
+  },
+  {
+    name: 'Railway',
+    icon: <SiRailway className="text-white " size={size} />,
+  },
+  {
+    name: 'Ubuntu',
+    icon: <SiUbuntu className="text-orange-500" size={size} />,
+  },
+];
+
+export default function Tech() {
+  const [techsList] = useState(techs);
+  const [showAll, setShowAll] = useState(false);
+
+  return (
+    <div>
+      <button onClick={() => setShowAll(!showAll)}>{showAll ? 'Show less' : 'Show all'}</button>
+
+      <div className="flex flex-wrap mt-8">
+        {techsList.slice(0, showAll ? techs.length : 6).map((skill, index) => (
+          <div
+            key={index}
+            className="flex flex-col items-center justify-center p-4 grayscale hover:filter-none transition duration-300 cursor-pointer"
+          >
+            {skill.icon}
+            <p className="text-white mt-2">{skill.name}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ src/lib/projects.ts
@@ -0,0 +1,36 @@
+interface Project {
+  name: string;
+  description: string;
+  link: string;
+}
+
+
+export const projects: Project[] = [
+  {
+    name: 'HRT Stack',
+    description:
+      'A full-stack web application stack for building high-quality and scalable web applications. Uses technologies like ono x TRPC for backend. React, Tailwind & Shadcn UI for frontend (with file based routing).',
+    link: 'https://github.com/lassejlv/hono-trpc',
+  },
+  {
+    name: 'NPM Package Search',
+    description: 'Nice and clean npm package searcher and viewer. Build using React, Tailwind CSS, Shadcn UI and the NPM API.',
+    link: 'https://github.com/lassejlv/npm-package-search',
+  },
+  {
+    name: 'Next Blog',
+    description:
+      'A blog with an admin panel for creating, updating and deleting posts. Uses Next.js, Drizzle ORM, Turso, React, Tailwind CSS and Shadcn UI.',
+    link: 'https://github.com/lassejlv/next-blog',
+  },
+  {
+    name: 'Actionfile',
+    description: 'Simplfied way to run tasks. Use an .toml file to define tasks and run them with the actionfile CLI.',
+    link: 'https://github.com/lassejlv/actionfile',
+  },
+  {
+    name: 'lassejlv.dk',
+    description: 'My personal website. Built with React, Tailwind CSS and Framer Motion.',
+    link: 'https://github.com/lassejlv/portfolio',
+  },
+];
